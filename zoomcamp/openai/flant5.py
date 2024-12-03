021e6e383fa2002cc7429aa76d8d6a7e0f8a1176
pmspraju <pmspraju@gmail.com>
2024-12-02 21:30:06 -0600

    1202

--- zoomcamp/openai/flant5.py
+++ zoomcamp/openai/flant5.py
@@ -1,20 +1,50 @@
 import os
 import gc
+import streamlit as st
+import time
 from mixedPrecision import ragModel
 
-os.environ['HF_HOME'] = '/home/nachiketa/Documents/Workspaces/HF_cache/flant5'
 
-# Load the data
-data_path = r'/home/nachiketa/Documents/Workspaces/LLM/zoomcamp/data'
-file = os.path.join(data_path, 'documents.json')
+def setPaths():
 
-#query = 'the course has already started, can I still enroll?'
-query = 'where are the main videos are stored'
-#query = 'when do i get the certificate'
-#query = 'how do I run kafka?'
+    os.environ['HF_HOME'] = '/home/nachiketa/Documents/Workspaces/HF_cache/flant5'
 
-gc.collect()
-answer = ragModel(file, query).rag()
-print(answer)
+    # Load the data
+    data_path = r'/home/nachiketa/Documents/Workspaces/LLMs/zoomcamp/data'
+    file = os.path.join(data_path, 'documents.json')
+
+    return file
+
+if __name__ == "__main__":
+
+    # set the paths and environment variables
+    file = setPaths()
+
+    # clear the cache
+    gc.collect()
+
+    # Set the query
+    # query = 'the course has already started, can I still enroll?'
+    #query = 'where are the main videos are stored'
+    # query = 'when do i get the certificate'
+    # query = 'how do I run kafka?'
+
+    # Get the answer
+    #answer = ragModel(file, query).rag()
+    #print(answer)
+
+    # show on the ui
+    st.title("RAG Function Invocation")
+    user_input = st.text_input("Enter your input:")
+
+    if st.button("Ask"):
+        with st.spinner('Processing...'):
+            output = ragModel(file, user_input).rag()
+            st.success("Completed!")
+            st.write(output)
 
 
+# Open a terminal.
+# Navigate to the directory where your Streamlit script is located.
+# Run the following command:
+#  streamlit run /home/nachiketa/Documents/Workspaces/LLMs/zoomcamp/openai/flant5.py
